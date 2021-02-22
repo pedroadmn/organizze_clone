6784c2289994105e02c996684dec67d11952f2cc
Pedro Adelino de Mello Neto <pedro.mello@virtus.ufcg.edu.br>
2021-02-22 16:22:02 -0300

    Adding expense and revenue screen UI

--- app/src/main/java/activities/HomeActivity.java
+++ app/src/main/java/activities/HomeActivity.java
@@ -1,5 +1,6 @@
 package activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -14,6 +15,9 @@ import pedroadmn.example.organizzeclone.R;
 
 public class HomeActivity extends AppCompatActivity {
 
+    private com.github.clans.fab.FloatingActionButton fabExpense;
+    private com.github.clans.fab.FloatingActionButton fabRevenue;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +25,18 @@ public class HomeActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show());*/
+        fabExpense = findViewById(R.id.fabExpense);
+        fabExpense.setOnClickListener(v -> goToExpense());
+
+        fabRevenue = findViewById(R.id.fabRevenue);
+        fabRevenue.setOnClickListener(v -> goToRevenue());
+    }
+
+    private void goToExpense() {
+        startActivity(new Intent(this, ExpenseActivity.class));
+    }
+
+    private void goToRevenue() {
+        startActivity(new Intent(this, RevenueActivity.class));
     }
 }
---
+++ app/src/main/java/activities/RevenueActivity.java
@@ -0,0 +1,16 @@
+package activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import pedroadmn.example.organizzeclone.R;
+
+public class RevenueActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_revenue);
+    }
+}
