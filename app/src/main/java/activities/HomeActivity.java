7555d7a6447d2a46442203750b68ccf49ee17014
Pedro Adelino de Mello Neto <pedro.mello@virtus.ufcg.edu.br>
2021-02-23 11:03:33 -0300

    adding home screen UI

--- app/src/main/java/activities/HomeActivity.java
+++ app/src/main/java/activities/HomeActivity.java
@@ -5,11 +5,15 @@ import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.prolificinteractive.materialcalendarview.CalendarDay;
+import com.prolificinteractive.materialcalendarview.MaterialCalendarView;
+import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.TextView;
 
 import pedroadmn.example.organizzeclone.R;
 
@@ -18,10 +22,20 @@ public class HomeActivity extends AppCompatActivity {
     private com.github.clans.fab.FloatingActionButton fabExpense;
     private com.github.clans.fab.FloatingActionButton fabRevenue;
 
+    private MaterialCalendarView calendarView;
+    private TextView tvGreetings;
+    private TextView tvBalance;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        tvGreetings = findViewById(R.id.tvGreetings);
+        tvBalance = findViewById(R.id.tvBalance);
+        calendarView = findViewById(R.id.calendarView);
+        setupCalendarView();
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -38,5 +52,13 @@ public class HomeActivity extends AppCompatActivity {
 
     private void goToRevenue() {
         startActivity(new Intent(this, RevenueActivity.class));
+    }
+
+    private void setupCalendarView() {
+        CharSequence months[] = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+        calendarView.setTitleMonths(months);
+        calendarView.setOnMonthChangedListener((widget, date) -> {
+
+        });
     }
 }
